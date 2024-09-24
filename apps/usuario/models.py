1af9e61b7b6c22c55845b117b1a984c41bc086ec
Tomas Coa <tomasjaviercm@gmail.com>
2024-09-24 01:33:05 -0300

    Creacion del modelo de usuario

--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from apps.rol.models import Rol
 
-# Create your models here.
+class Usuario(models.Model):
+    rol = models.ForeignKey(Rol, on_delete=models.SET_NULL, null=True)
+    nombre = models.CharField(max_length=150)
+    apellido = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    contraseña = models.CharField(max_length=128)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    direccion_real = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido} ({self.email})"
