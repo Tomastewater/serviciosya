cb01580d84c1272a1b647cdb101e787be0c83847
Tomas Coa <tomasjaviercm@gmail.com>
2024-09-27 19:12:32 -0300

    Modificacion de de app usuario, se elimino el Rol y se removio el consumidor

--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -1,15 +1,8 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password
 
-class Rol(models.Model):    # ¿Es mejor crear una app llamada rol que contenga a consumidor y a prestador?
-    nombre = models.CharField(max_length=100)       #CharField es mas eficiente porque esta optimizado para textos cortos
-    descripcion = models.TextField(max_length=500)
-
-    def __str__(self) -> str:
-        return f'Nombre: {self.nombre} \nDescripcion: {self.descripcion}'
-
 class Usuario(models.Model):
-    rol = models.ForeignKey(Rol, on_delete=models.DO_NOTHING, null=False)
+    rol = models.enums["Consumidor", "Prestador"]
     nombre = models.CharField(max_length=150)
     apellido = models.CharField(max_length=150)
     email = models.EmailField(unique=True)
@@ -27,8 +20,3 @@ class Usuario(models.Model):
         self.contraseña = make_password(password)
         self.save()
 
-class Consumidor(models.Model):
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    # descuentos
-    def __str__(self):
-        return f'Nombre Consumidor: {self.usuario.nombre} {self.usuario.apellido}'
