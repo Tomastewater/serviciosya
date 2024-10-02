53362f2f02f44749b331a2ee1e62969bd552cb2a
Tomas Coa <tomasjaviercm@gmail.com>
2024-10-01 23:12:39 -0300

    Se realizo el model de de la app pago, agregando las clases MetodoPago, Estado y Pago

--- apps/pago/models.py
+++ apps/pago/models.py
@@ -1,4 +1,26 @@
 from django.db import models
 
-# Create your models here.
-#cualquier cosa 
+class MetodoPago(models.Model):
+    nombre = models.CharField(max_length=100)
+    detalle = models.TextField(max_length=500)
+
+    def __str__(self) -> str:
+        return f"Metodo de pago: {self.nombre} \nDetalle: {self.detalle}"
+    
+class Estado(models.Model):
+    nombre = models.CharField(max_length=100)
+    detalle = models.TextField(max_length=500)
+
+    def __str__(self):
+        return f"Estado del pago: {self.nombre} \nDetalles: {self.detalle}"
+    
+
+class Pago(models.Model):
+    fecha_pago = models.DateField()
+    monto_pago = models.DecimalField(max_digits=15, decimal_places=2)
+    metodo_pago = models.ForeignKey(MetodoPago, on_delete=models.CASCADE)
+    estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
+    detalles = models.TextField(max_length=1000)
+    
+    def __str__(self) -> str:
+        return f"Fecha del pago: {self.fechaPago} \nMonto pagado: {self.montoPago} \nMetodo de pago: {self.metodoPago.nombre} \nEstado del pago: {self.estado.nombre} \nDetalles del pago: {self.detalles}"
