2dc8f1a4e8234dedbd545783d8828511b37e5493
Tomas Coa <tomasjaviercm@gmail.com>
2024-10-02 23:10:08 -0300

    Realizacion del modelo de calificacion

--- apps/calificacion/models.py
+++ apps/calificacion/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Calificacion(models.Model):
+    CALIFICACION_OPCIONES = [
+        (1,'1 - Muy malo'),
+        (2,'2 - Malo'),
+        (3,'3 - Regular'),
+        (4,'4 - Bueno'),
+        (5,'5 - Excelente'),
+    ]
+
+    fecha_calificacion = models.DateField()
+    Calificacion = models.IntegerField(choices=CALIFICACION_OPCIONES)
+    comentario = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return f"Fecha de la Calificacion: {self.fecha_calificacion} \nCalificacion: {self.Calificacion}/5 \nComentario: {self.comentario}"
+    
