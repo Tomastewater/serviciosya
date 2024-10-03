3ee1b60112f5d071d6d141efb6ece1b268218985
Tomas Coa <tomasjaviercm@gmail.com>
2024-10-03 00:03:40 -0300

    Se realizaron las migraciones correctamente

--- apps/facturacion/models.py
+++ apps/facturacion/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Factura(models.Model):
     fecha_emision = models.DateField()
-    impuestos = models.DecimalField(max_digits=15)
-    monto_total = models.DecimalField(max_digits=15)
+    impuestos = models.DecimalField(max_digits=15, decimal_places=2)
+    monto_total = models.DecimalField(max_digits=15, decimal_places=2)
     detalles = models.TextField(max_length=500)
 
     def __str__(self) -> str:
