de48f425967024d9baa0b00bf0273198d8aa0823
Tomas Coa <tomasjaviercm@gmail.com>
2024-10-02 23:28:10 -0300

    Se completo el models de la app Consumidor

--- apps/consumidor/models.py
+++ apps/consumidor/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Consumidor(models.Model):
+    usuario = models.ForeignKey('usuario.Usuario', on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        """Devuelve una representación en cadena (string) de la instancia del modelo Consumidor.
+
+        Si el consumidor tiene un usuario asociado, devuelve el nombre completo del usuario (nombre y apellido).
+        Si el consumidor no tiene un usuario asociado, devuelve un mensaje indicando que no hay usuario asociado.
+        """
+        if self.usuario:
+            return f'Nombre Consumidor: {self.usuario.nombre} {self.usuario.apellido}'
+        return 'Consumidor sin usuario asociado'
