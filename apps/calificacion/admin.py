596c3aba0f0aac7e734ebb7830af53c3aa61ef96
Tomas Coa <tomasjaviercm@gmail.com>
2024-10-18 21:20:38 -0300

    Admin en calificacion

--- apps/calificacion/admin.py
+++ apps/calificacion/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from .models import Calificacion
 
-admin.site.register(Calificacion)
+@admin.register(Calificacion)    # Los decoradores son fucniones que haran ALGO antes o despues de la clase
+class calificacionAdmin(admin.ModelAdmin):
+    list_display = []   # Que queremos mostrar
+    search_fields = []  # lo Que queremos buscar  (Podemos buscar por los datos correspondiente a los campos)
+    list_filter = []  # que queremos filtrar
 
-
+# Buscar acerca de Admin Actions
