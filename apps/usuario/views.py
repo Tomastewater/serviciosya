d33189841467c41f1dd0befe738828861237887b
Tomas Coa <tomasjaviercm@gmail.com>
2024-11-24 22:05:28 -0300

    Login redirige automaticamente a un usuario dependiendo de su rol

--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -1,14 +1,12 @@
 from django.views import generic
 from django.urls import reverse_lazy
+from .models import Rol
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.db.models import Q
-
 from .form import UsuarioForm
-
-#from apps.ubicacion.form import direccionForm
 from apps.servicio.models import Servicio, Categoria, ServicioPrestado
 
 class usuarioFormView(generic.FormView):
@@ -30,18 +28,30 @@ class aboutView(generic.TemplateView):
     template_name = 'about.html'
 
 class CustomLoginView(generic.View):
+
     def get(self, request, *args, **kwargs):
         form = AuthenticationForm()
         return render(request, 'registration/login.html', {'form': form})
 
     def post(self, request, *args, **kwargs):
         form = AuthenticationForm(request, data=request.POST)
+        
         if form.is_valid():
             # Si el formulario es válido, autenticamos al usuario
-            user = form.get_user()
-            login(request, user)
-            # messages.success(request, '¡Bienvenido! Has iniciado sesión correctamente.')
-            return redirect(reverse_lazy('consumidor'))  # Redirige a la vista de consumidor
+            usuario = form.get_user()
+            login(request, usuario)
+            
+            # Determinar el rol del usuario
+            try:
+                rol_usuario = usuario.roles.first()  # Obtener el primer rol asociado al usuario
+                if rol_usuario.rol == 1:  # Consumidor
+                    return redirect(reverse_lazy('consumidor'))
+                elif rol_usuario.rol == 2:  # Prestador
+                    return redirect(reverse_lazy('prestador'))
+            except Rol.DoesNotExist:
+                messages.error(request, 'No se encontró un rol asociado al usuario.')
+                return redirect(reverse_lazy('login'))  # Redirige al login en caso de error
+
         else:
             # Si no es válido, mostramos un mensaje de error
             messages.error(request, 'Error en las credenciales, por favor intenta nuevamente.')
@@ -103,44 +113,4 @@ class ServicioDetailView(generic.DetailView):
             'prestador', 'localidad', 'servicio'
         ).filter(servicio=self.object)
         return context
-
-"""
-class consumidorView(LoginRequiredMixin, generic.TemplateView):
-    template_name = 'index_consumidor.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
         
-        # Obtener el usuario autenticado
-        usuario = self.request.user
-
-        contratos = Contrato.objects.filter(direccion__usuario=usuario)  
-        direcciones = Direccion.objects.filter(usuario=usuario)
-        
-        context['contratos'] = contratos
-        context['direcciones'] = direcciones
-        context['usuario'] = usuario
-
-        return context
-    
-        
-class direccionView(generic.TemplateView):
-    template_name = 'directions.html'
-
-    def get_context_data(self, **kwargs):
-
-        usuario = self.request.user
-
-        context =  super().get_context_data(**kwargs)
-        context['direcciones'] = Direccion.objects.filter(direccion__usuario =usuario)
-        context['usuarios'] = usuario
-        return context
-class ubicacionFormView(generic.FormView):
-    template_name = 'formUbicacion.html'
-    form_class = direccionForm
-    success_url = reverse_lazy('oneDirection')
-
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
-"""        
