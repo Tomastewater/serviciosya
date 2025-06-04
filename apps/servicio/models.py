572d53c8c1798637fc0d14a4d2b3056f4c8959a1
claudiasantivaniez <santivaniezclaudia@gmail.com>
2025-06-04 20:38:56 -0300

    se agrega el delete de la imagen al eliminar el servicio

--- apps/servicio/models.py
+++ apps/servicio/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+import os
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=200)
@@ -23,7 +26,11 @@ class ServicioPrestado(models.Model):
     imagen = models.ImageField(upload_to='servicios_prestados/', null=True, blank=True)
     descripcion = models.TextField(max_length=500, null=True, blank=True)
 
-
     def __str__(self) -> str:
         return f'{self.categoria.nombre} - Prestado por: {self.prestador.rol_usuario.usuario.nombre} {self.prestador.rol_usuario.usuario.apellido} en {self.localidad}'
 
+@receiver(post_delete, sender=ServicioPrestado)
+def eliminar_imagen_servicio(sender, instance, **kwargs):
+    if instance.imagen and instance.imagen.path:
+        if os.path.isfile(instance.imagen.path):
+            os.remove(instance.imagen.path)
